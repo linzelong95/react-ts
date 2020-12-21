a0f1217181228db7331f756c012576c356315466
linzelong95 <linzelong95@163.com>
2020-12-21 18:06:42 +0800

    chore: format

--- webpack/build.js
+++ webpack/build.js
@@ -2,13 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const execa = require('execa')
 
-// 打包
-// npm run build 打包本项目（项目所需的基本包单独打包）
-// npm run build base  打包基础包（只有在版本升级的时候才需要打包，通常来说打包一次就够了）
-
-// 安装依赖
-// 使用npm ci来安装依赖
-
 const modules = process.argv.slice(3)
 const mode = process.argv[2]
 const cwd = path.resolve(__dirname, '../')
